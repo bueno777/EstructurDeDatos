c64f98bf046822bca4c91158740d355b231ccace
bueno777 <37460294+bueno777@users.noreply.github.com>
2021-01-20 14:04:47 -0700

    Entrega de primer projecto
    Conceptos Basicos

---
+++ ConceptosBasicos/src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextField txtEdad;
+    @FXML
+    Label lblTitulo;
+
+    public void event(ActionEvent event) {
+        int dias = Integer.parseInt(txtEdad.getText())* 365;
+            lblTitulo.setText(" has vivido  " + dias + " dias ");
+
+        }
+    }
+
+
+
